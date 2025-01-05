c2da38e9df22715f047786a32572c5d355218805
Amat Raci <amat.raci@student.uni-pr.edu>
2025-01-05 22:04:36 +0100

    shtimi i try and catch

--- exhibition-events/exhibitions/exhibition-events.js
+++ exhibition-events/exhibitions/exhibition-events.js
@@ -1,53 +1,63 @@
 $(document).ready(function () {
-    $(".image-gallery img").click(function () {
-        if ($("#image-modal").length === 0) {
-            $("body").append(`
-                <div id="image-modal" class="image-modal">
-                    <span class="close">&times;</span>
-                    <img class="modal-content" id="modal-image">
-                </div>
-            `);
-        }
+    try {
+        $(".image-gallery img").click(function () {
+            try {
+                if ($("#image-modal").length === 0) {
+                    $("body").append(`
+                        <div id="image-modal" class="image-modal">
+                            <span class="close">&times;</span>
+                            <img class="modal-content" id="modal-image">
+                        </div>
+                    `);
+                }
 
-        const modal = $("#image-modal");
-        const modalImage = $("#modal-image");
+                const modal = $("#image-modal");
+                const modalImage = $("#modal-image");
 
-        modalImage.attr("src", $(this).attr("src"));
-        modal.fadeIn();
+                modalImage.attr("src", $(this).attr("src"));
+                modal.fadeIn();
 
-        $(".close").click(function () {
-            modal.fadeOut();
-        });
+                $(".close").click(function () {
+                    modal.fadeOut();
+                });
 
-        modal.click(function (e) {
-            if (!$(e.target).is(".modal-content")) {
-                modal.fadeOut();
+                modal.click(function (e) {
+                    if (!$(e.target).is(".modal-content")) {
+                        modal.fadeOut();
+                    }
+                });
+            } catch (err) {
+                console.error("Error in image gallery functionality:", err);
             }
         });
-    });
+    } catch (err) {
+        console.error("Error in document ready function:", err);
+    }
+
+    try {
+        function executeWithCallback(data, callback) {
+            console.log(`Processing data: ${data}`);
+            callback(data);
+        }
+
+        function processData(data) {
+            console.log(`Data processed: ${data}`);
+        }
+
+        executeWithCallback("Sample Data", processData);
+
+        const myButton = document.createElement("button");
+        myButton.innerText = "Click Me";
+        document.body.appendChild(myButton);
+
+        myButton.addEventListener("click", function () {
+            console.log("Button clicked!");
+        });
+
+        function filterCatalog(category) {
+            sessionStorage.setItem("selectedCategory", category);
+        }
+    } catch (err) {
+        console.error("Error in other functions:", err);
+    }
 });
-
-function executeWithCallback(data, callback) {
-    console.log(`Processing data: ${data}`);
-    callback(data);
-}
-
-
-function processData(data) {
-    console.log(`Data processed: ${data}`);
-}
-
-
-executeWithCallback("Sample Data", processData);
-
-const myButton = document.createElement("button");
-myButton.innerText = "Click Me";
-document.body.appendChild(myButton);
-
-myButton.addEventListener("click", function () {
-    console.log("Button clicked! ");
-});
-
-function filterCatalog(category) {
-    sessionStorage.setItem("selectedCategory", category);
-}

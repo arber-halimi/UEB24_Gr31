e84d5989a0d6fe721c3f9c8a056dfa88f1e0a7e6
Amat Raci <amat.raci@student.uni-pr.edu>
2025-01-05 20:31:13 +0100

    editim i js

--- exhibition-events/exhibitions/exhibition-events.js
+++ exhibition-events/exhibitions/exhibition-events.js
@@ -1,35 +1,53 @@
- $(document).ready(function () {
-            $(".image-gallery img").click(function () {
-                
-                if ($("#image-modal").length === 0) {
-                    $("body").append(`
-                        <div id="image-modal" class="image-modal">
-                            <span class="close">&times;</span>
-                            <img class="modal-content" id="modal-image">
-                        </div>
-                    `);
-                }
-           
-                const modal = $("#image-modal");
-                const modalImage = $("#modal-image");
-    
-                modalImage.attr("src", $(this).attr("src"));
-                modal.fadeIn();
-    
-                $(".close").click(function () {
-                    modal.fadeOut();
-                });
-    
-                modal.click(function (e) {
-                    if (!$(e.target).is(".modal-content")) {
-                        modal.fadeOut();
-                    }
-                });
-            });
+$(document).ready(function () {
+    $(".image-gallery img").click(function () {
+        if ($("#image-modal").length === 0) {
+            $("body").append(`
+                <div id="image-modal" class="image-modal">
+                    <span class="close">&times;</span>
+                    <img class="modal-content" id="modal-image">
+                </div>
+            `);
+        }
+
+        const modal = $("#image-modal");
+        const modalImage = $("#modal-image");
+
+        modalImage.attr("src", $(this).attr("src"));
+        modal.fadeIn();
+
+        $(".close").click(function () {
+            modal.fadeOut();
+        });
+
+        modal.click(function (e) {
+            if (!$(e.target).is(".modal-content")) {
+                modal.fadeOut();
+            }
+        });
+    });
+});
+
+function executeWithCallback(data, callback) {
+    console.log(`Processing data: ${data}`);
+    callback(data);
+}
+
+
+function processData(data) {
+    console.log(`Data processed: ${data}`);
+}
+
+
+executeWithCallback("Sample Data", processData);
+
+const myButton = document.createElement("button");
+myButton.innerText = "Click Me";
+document.body.appendChild(myButton);
+
+myButton.addEventListener("click", function () {
+    console.log("Button clicked! ");
 });
 
 function filterCatalog(category) {
-    // Save the filter category to sessionStorage
-    sessionStorage.setItem('selectedCategory', category);
-  }
-  
+    sessionStorage.setItem("selectedCategory", category);
+}
